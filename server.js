9c22f5b287f3cf9554e0244c8df4ef2a6abb3341
Johann Lee <jhnlee0526@gmail.com>
2020-07-03 14:47:43 -0700

    working on proxy dockerize

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
-// const express = require('express')
-// const app = express()
-// const port = 3000
-
-// app.use(express.static('./'))
-
-// app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const url = `http://localhost:${port}`;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/')));
-app.use('/checkout', createProxyMiddleware({ target: 'http://localhost:3003/', changeOrigin: true }));
-app.use('/reviews', createProxyMiddleware({ target: 'http://localhost:3009/', changeOrigin: true }));
-app.use('/carousel', createProxyMiddleware({ target: 'http://localhost:3007', changeOrigin: true }));
+app.use('/checkout', createProxyMiddleware({ target: 'http://18.191.143.205/', changeOrigin: true }));
+app.use('/reviews', createProxyMiddleware({ target: 'http://18.144.83.187/', changeOrigin: true }));
+app.use('/carousel', createProxyMiddleware({ target: 'http://54.193.94.230/', changeOrigin: true }));
 app.listen(port, () => {
   console.log(`Listening at ${url}`);
 });
